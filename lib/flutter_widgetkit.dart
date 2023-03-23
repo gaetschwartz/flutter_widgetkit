008bac0ec490f4235b23fd066e4227ee9ecd40b8
Gaëtan <blaxoujunior@gmail.com>
2023-03-23 12:30:36 +0100

    if i speak

--- lib/flutter_widgetkit.dart
+++ lib/flutter_widgetkit.dart
@@ -1,34 +1,32 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class WidgetKit {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_widgetkit');
+  static const MethodChannel _channel = MethodChannel('flutter_widgetkit');
 
-  // static Future<String> get platformVersion async {
-  //   final String version = await _channel.invokeMethod('getPlatformVersion');
-  //   return version;
-  // }
-
-  static void reloadAllTimelines() async {
+  static Future<void> reloadAllTimelines() async {
     await _channel.invokeMethod('reloadAllTimelines');
   }
 
-  static void reloadTimelines(String ofKind) async {
-    await _channel.invokeMethod('reloadTimelines', <String, dynamic>{'ofKind': ofKind});
+  static Future<void> reloadTimelines(String ofKind) async {
+    await _channel
+        .invokeMethod('reloadTimelines', <String, dynamic>{'ofKind': ofKind});
   }
 
-  static dynamic getItem(String key, String appGroup) async {
-    return await _channel.invokeMethod('getItem', <String, dynamic>{'key': key, 'appGroup': appGroup});
+  static Future<dynamic> getItem(String key, String appGroup) async {
+    return await _channel.invokeMethod(
+        'getItem', <String, dynamic>{'key': key, 'appGroup': appGroup});
   }
 
-  static dynamic setItem(String key, dynamic value, String appGroup) async {
-    return await _channel.invokeMethod('setItem', <String, dynamic>{'key': key, 'value': value, 'appGroup': appGroup});
+  static Future<dynamic> setItem(
+      String key, dynamic value, String appGroup) async {
+    return await _channel.invokeMethod('setItem',
+        <String, dynamic>{'key': key, 'value': value, 'appGroup': appGroup});
   }
 
-  static Future<bool> removeItem(String key, String appGroup) async {
-    return await _channel.invokeMethod('removeItem', <String, dynamic>{'key': key, 'appGroup': appGroup});
+  static Future<bool?> removeItem(String key, String appGroup) async {
+    return await _channel.invokeMethod<bool>(
+        'removeItem', <String, dynamic>{'key': key, 'appGroup': appGroup});
   }
 }
